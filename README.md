fab2182a91154a5a0e5ad5281dcff2d26ffea3ba
Nikolas Dahn <nikolas.dahn@gmail.com>
2025-02-23 15:19:33 +0100

    Initial commit, already working!

---
+++ README.md
@@ -0,0 +1,25 @@
+A nifty little tool I wrote for transfering sounds between Elden Ring soundbanks. 
+
+# What does it do?
+You pass the script two unpacked soundbanks and the sound IDs you want to use, and it will copy the JSON entries and WEMs from the first soundbank to the second. This is essentially what is described in [Themyys' tutorial](http://soulsmodding.wikidot.com/tutorial:main#toc9).
+
+# How do I use it?
+More specifically, you need to have 3 things:
+- an *unpacked* source soundbank, typically from an NPC or map.
+- an *unpacked* destination soundbank, typically *cs_main.bnk*.
+- the wwise IDs of the sounds you want to use (e.g. in animation TAEs).
+
+To unpack your game use [UXM](https://github.com/Nordgaren/UXM-Selective-Unpack). You *have to* unpack the entire game, otherwise the soundbanks will not be included. Soundbanks can be found under `sd` or `sd/enus` and unpacked using [rewwise](https://github.com/vswarte/rewwise/) by dropping them onto `bnk2json.exe`.
+
+To use the script, either modify the uppercase variables at the top, or call it from a terminal. See `--help` for required arguments and such. Paths to soundbanks should lead to the unpacked directories (not e.g. the `soundbank.json` inside). If a passed path is not absolute, the script's folder will be used; that is, you can place your unpacked soundbanks in the same folder as the script to avoid having to type full paths.
+
+# Fine tuning
+You may want to do some small adjustments to your soundfiles, e.g. setting a different volume. This process is still entirely manual and up to you. This is done in the edited `soundbank.json`. Look for the IDs the script printed during execution; the volume is usually adjusted on a `RandomSequenceContainer`. It will contain a dict called `node_initial_params` in which you should find the relevant settings. There are different (*mysterious*) ways of influencing audio volume (such as *GameAuxSendVolume*, *UserAuxSendVolume0*, *GameAuxSendVolume*, etc.), but the easiest (and recommended!) one to use is just called `Volume`. Values will typically range from `-15` for very loud noises and `-3` for very quiet ones. 
+
+# Using modded sound files
+Once the script is done (assuming `enable_write` was set, of course), a backup of the destination `soundbank.json` will be placed in the destination's *parent* folder (look for `<soundbank_name>_backup.json`). You will have to pack your soundbank again using *rewwise* by dropping the modified soundbank folder onto `bnk2json.exe`. Then place the *repacked* soundbank in your mod folder, in a path corresponding to where you got it from (if you got it from `sd/enus`, place it in `sd/enus`, etc.).
+
+The official release of [Mod Engine 2](https://github.com/soulsmods/ModEngine2) does not support loading modified sound files right now. There is an inofficial release floating around which is packaged with some mods, e.g. [this one](https://www.nexusmods.com/eldenring/mods/6384) or [this one](https://www.nexusmods.com/eldenring/mods/6340). Don't forget to give an "endorse" to these kind people :) Also, you can keep your previous config files!
+
+# Caveats
+It probably also works for Dark Souls 3, Sekiro and more, but I have never tested it.
---
+++ er_soundbank_helper.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python3
+import sys
+from types import MappingProxyType
+import traceback
+from pathlib import Path
+import shutil
+import json
+from pprint import pprint
+
+
+SRC_BNK_JSON = "cs_c4770"
+DST_BNK_JSON = "cs_main"
+SOUND_IDS = [
+    477008001,
+]
+ENABLE_WRITE = True
+
+
+def load_indices(
+    bnk: dict,
+) -> tuple[list[dict], MappingProxyType[int, int], MappingProxyType[str, int]]:
+    sections = bnk.get("sections", None)
+
+    if not sections:
+        raise ValueError("Could not find 'sections' in bnk")
+
+    for sec in sections:
+        if "HIRC" in sec["body"]:
+            hirc: list[dict] = sec["body"]["HIRC"]["objects"]
+            break
+    else:
+        raise ValueError("Could not find HIRC in bnk")
+
+    objects = {}
+    events = {}
+    for idx, obj in enumerate(hirc):
+        idsec = obj["id"]
+        if "Hash" in idsec:
+            oid = idsec["Hash"]
+            objects[oid] = idx
+        elif "String" in idsec:
+            eid = idsec["String"]
+            events[eid] = idx
+        else:
+            print(f"Don't know how to handle object with id {idsec}")
+
+    # MappingProxyType is our immutable dict
+    return hirc, MappingProxyType(objects), MappingProxyType(events)
+
+
+def main(
+    src_bnk_path: str,
+    dst_bnk_path: str,
+    sound_ids: list[int],
+    enable_write: bool = True,
+):
+    src_bnk_path: Path = Path(src_bnk_path)
+    dst_bnk_path: Path = Path(dst_bnk_path)
+
+    if not src_bnk_path.is_absolute():
+        src_bnk_path = Path(__file__).resolve().parent / src_bnk_path
+
+    if not dst_bnk_path.is_absolute():
+        dst_bnk_path = Path(__file__).resolve().parent / dst_bnk_path
+
+    src_bnk_path = src_bnk_path.resolve()
+    dst_bnk_path = dst_bnk_path.resolve()
+
+    print("Loading source soundbank")
+    src_json = src_bnk_path / "soundbank.json"
+    with src_json.open() as f:
+        src_bnk: dict = json.load(f)
+
+    print("Loading destination soundbank")
+    dst_json = dst_bnk_path / "soundbank.json"
+    with dst_json.open() as f:
+        dst_bnk: dict = json.load(f)
+
+    src_hirc, src_oid_to_idx, src_eid_to_idx = load_indices(src_bnk)
+    dst_hirc, dst_oid_to_idx, _ = load_indices(dst_bnk)
+    wems = []
+
+    print("Collecting sound hierarchies")
+    for snd in sound_ids:
+        # Just for debugging
+        debug_tree = {}
+
+        # Find the play and stop events. The actual action comes right before the event, but
+        # we could also find their ID via body/Event/actions[0] for more robustness
+        play_evt_idx = src_eid_to_idx.get(f"Play_c{snd}", None)
+        if play_evt_idx is None:
+            raise ValueError(f"Could not find {snd} in source soundbank")
+
+        stop_evt_idx = src_eid_to_idx[f"Stop_c{snd}"]
+
+        # Add the play/stop events and actions
+        transfer_object_indices = [
+            play_evt_idx - 1,
+            play_evt_idx,
+            stop_evt_idx - 1,
+            stop_evt_idx,
+        ]
+
+        # Find the container the action is triggering, then go up the hierarchy until we find
+        # the ActorMixer responsible for playback
+        parent_id = src_hirc[play_evt_idx - 1]["body"]["Action"]["external_id"]
+        parent_idx = src_oid_to_idx[parent_id]
+        actor_mixer_id = None
+        snd_sc_root_id = None
+
+        while True:
+            node = src_hirc[parent_idx]
+            node_type = next(iter(node["body"].keys()))
+            node_params = node["body"][node_type]
+            oid = next(iter(node["id"].values()))
+
+            # The ActorMixer is where our hierarchy will be included from
+            if node_type == "ActorMixer":
+                actor_mixer_id = oid
+                break
+
+            if node_type == "Sound":
+                wem = node_params["bank_source_data"]["media_information"]["source_id"]
+                wems.append(wem)
+
+            if "children" in node_params:
+                children = node_params["children"].get("items", [])
+                debug_children = debug_tree.setdefault("children", [])
+                
+                for child_id in children:
+                    child_idx = src_oid_to_idx[child_id]
+                    transfer_object_indices.append(child_idx)
+
+                    # Assume that all sound objects will be leafs
+                    child = src_hirc[child_idx]
+                    child_type = next(iter(child["body"].keys()))
+                    if child_type == "Sound":
+                        wem = child["body"]["Sound"]["bank_source_data"]["media_information"]["source_id"]
+                        wems.append(wem)
+
+                    debug_children.append(f"{child_type} ({child_id})")
+                
+            transfer_object_indices.append(parent_idx)
+            
+            debug_tree = {f"{node_type} ({oid})": debug_tree}
+
+            # Go up the hierarchy until we find an ActorMixer
+            snd_sc_root_id = parent_id
+            parent_id = node_params["node_base_params"]["direct_parent_id"]
+            parent_idx = src_oid_to_idx[parent_id]
+
+        if actor_mixer_id is None:
+            raise ValueError("ActorMixer could not be found")
+
+        print(f"parsing {snd} resulted in the following hierarchy:")
+        pprint(debug_tree)
+
+        print("The following wems were collected:")
+        pprint(wems)
+
+        # Check if the ActorMixer already exists in the destination soundbank
+        if actor_mixer_id in dst_oid_to_idx:
+            transfer_idx = dst_oid_to_idx[actor_mixer_id]
+            dst_actor_mixer = dst_hirc[transfer_idx]
+
+            print(
+                f"Inserting {len(transfer_object_indices)} nodes before actor mixer {actor_mixer_id}"
+            )
+        else:
+            # ActorMixer does not exist yet, copy it below the first SC
+            actor_mixer_idx = src_oid_to_idx[actor_mixer_id]
+            dst_actor_mixer = src_hirc[actor_mixer_idx]  # not a copy, but should be okay
+            dst_actor_mixer["body"]["ActorMixer"]["children"]["items"] = []
+            transfer_object_indices.append(src_oid_to_idx[actor_mixer_id])
+
+            transfer_idx = -1
+            for idx, obj in enumerate(dst_hirc):
+                obj_type = next(iter(obj["body"].keys()))
+                if obj_type == "RandomSequenceContainer":
+                    transfer_idx = idx + 1
+                    break
+            
+            print(f"Copying ActorMixer {actor_mixer_id} and {len(transfer_object_indices) - 1} nodes")
+
+        for idx in transfer_object_indices:
+            obj = src_hirc[idx]
+            oid = next(iter(obj["id"].values()))
+
+            if oid in dst_oid_to_idx:
+                while True:
+                    reply = input(
+                        f"Object ID {oid} already exists in target soundbank. "
+                        "[s]kip, [c]ancel, [w]rite? > "
+                    )
+                    if reply == "s":
+                        # skip
+                        continue
+                    if reply == "c":
+                        # cancel everything
+                        sys.exit(-1)
+                    if reply == "w":
+                        # write anyways
+                        break
+
+            dst_hirc.insert(transfer_idx, obj)
+            transfer_idx += 1
+
+        # Add the inserted hierarchy to the ActorMixer in the destination
+        dst_am_children: list = dst_actor_mixer["body"]["ActorMixer"]["children"]
+        dst_am_children_items = dst_am_children.setdefault("items", [])
+        dst_am_children_items.append(snd_sc_root_id)
+        dst_am_children_items.sort()
+
+    print("All hierarchies collected")
+
+    if not enable_write:
+        print(
+            f"enable_write is False, no changes to the target soundbank or wems will be made"
+        )
+    else:
+        print(f"Writing destination soundbank ({len(dst_hirc)} nodes)")
+        # Replace the original hirc in the destination soundbank
+        dst_sections = dst_bnk["sections"]
+        for idx, sec in enumerate(dst_sections):
+            if "HIRC" in sec["body"]:
+                sec["body"]["HIRC"]["objects"] = dst_hirc
+                break
+
+        backup = dst_bnk_path.name.rsplit(".", maxsplit=1)[0] + "_backup.json"
+        shutil.move(dst_json, dst_bnk_path.parent / backup)
+        with dst_json.open("w") as f:
+            json.dump(dst_bnk, f, indent=2)
+
+        print(f"Copying {len(wems)} wems")
+        for wem in wems:
+            wem_name = f"{wem}.wem"
+            if (dst_bnk_path / wem_name).is_file():
+                print(f"wem {wem_name} already exists, skipping")
+            else:
+                shutil.copy(src_bnk_path / wem_name, dst_bnk_path / wem_name)
+
+    input("\nDone! Press Enter to exit")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        src_bnk = SRC_BNK_JSON
+        dst_bnk = DST_BNK_JSON
+        sound_ids = SOUND_IDS
+        enable_write = ENABLE_WRITE
+    else:
+        import argparse
+
+        parser = argparse.ArgumentParser(description="A nifty tool for transfering wwise sounds between From software soundbanks.")
+
+        parser.add_argument("src_bnk", type=str, help="The source soundbank folder")
+        parser.add_argument("dst_bnk", type=str, help="The destination soundbank folder")
+        parser.add_argument("sound_ids", type=int, nargs="+", help="The wwise sound IDs you wish to transfer")
+        parser.add_argument("--disable_write", action="store_true", help="If True, no changes to the destination soundbank will be made")
+
+        args = parser.parse_args()
+
+        if args.help:
+            parser.print_help()
+            sys.exit(1)
+
+        src_bnk = args.src_bnk
+        dst_bnk = args.dst_bnk
+        sound_ids = args.sound_ids
+        enable_write = not args.disable_write
+
+    try:
+        main(src_bnk, dst_bnk, sound_ids, enable_write)
+    except Exception as e:
+        if hasattr(sys, "gettrace") and sys.gettrace() is not None:
+            # Debugger is active, let the debugger handle it
+            raise
+
+        # In case we are run from a temporary terminal, otherwise we won't see what's wrong
+        print(traceback.format_exc())
+        input("\nPress enter to exit")
